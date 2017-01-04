29b4730fa57895e0ea55802193d86b8721fb775d
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-01-04 15:01:45 +0800

    1,tapstop change adjust. 2,for vmUsb,out the select for member

--- kvm/vmStart.sh
+++ kvm/vmStart.sh
@@ -1,108 +1,108 @@
 #!/bin/sh
 #howToUse:
-	#1,configIterm need Modify
-		#xmlTemplate
-		#domainSavedDir
-		#imgSizeWhenAutoCreate='25G'
-	#2,./vmStart.sh *.xml or *.iso
+    #1,configIterm need Modify
+        #xmlTemplate
+        #domainSavedDir
+        #imgSizeWhenAutoCreate='25G'
+    #2,./vmStart.sh *.xml or *.iso
 
 #depend on
-	#1,qemu-img to gen imgDisk
-	#2,awk/sed/python
-	#3,netstat auto find vncport
-	#4,i3-msg adjust win
-	#5,uuidgen,auto gen uuid
+    #1,qemu-img to gen imgDisk
+    #2,awk/sed/python
+    #3,netstat auto find vncport
+    #4,i3-msg adjust win
+    #5,uuidgen,auto gen uuid
 
 function Usage(){
-	echo "need param"
-	echo "Usage: $program 'domain'"
+    echo "need param"
+    echo "Usage: $program 'domain'"
 }
 
 function verbose(){
-	[ $DEBUG == "yes" ] && {  $@; }
+    [ $DEBUG == "yes" ] && {  $@; }
 }
 
 function domainCreate(){
-	local ret;
-	if [ -r $domainSaved ];then
-		verbose echo "restore domain $domainSaved"
-		$lvirsh restore $domainSaved
-		ret=$?
-		rm -rf $domainSaved
-	else
-		verbose echo "define&start domain from $xmlConfig"
-		$lvirsh define $xmlConfig
-		$lvirsh start ${domain}
-		ret=$?
-	fi
+    local ret;
+    if [ -r $domainSaved ];then
+        verbose echo "restore domain $domainSaved"
+        $lvirsh restore $domainSaved
+        ret=$?
+        rm -rf $domainSaved
+    else
+        verbose echo "define&start domain from $xmlConfig"
+        $lvirsh define $xmlConfig
+        $lvirsh start ${domain}
+        ret=$?
+    fi
 
-	if [ $ret -eq 0 ];then
-		echo -e "\033[1;32mdomain $xmlConfig up\033[0m"
-	else
-		echo -e "\033[1;31mdomain $xmlConfig up failed,exit -1\033[0m"
-		exit -1
-	fi
+    if [ $ret -eq 0 ];then
+        echo -e "\033[1;32mdomain $xmlConfig up\033[0m"
+    else
+        echo -e "\033[1;31mdomain $xmlConfig up failed,exit -1\033[0m"
+        exit -1
+    fi
 }
 
 #confirm vncPort for domain throw /proc/$pid/cmdline
 function gotDomainVncPort(){
-	local pid port pids
-	local pidPorts=( )
-	oldIFS="$IFS"
-	#allVnc=`sudo netstat -nptl 2>/dev/null | grep ":59[0-9][0-9]"`
-	tmp=`$lnetstat -nptl 2>/dev/null`
+    local pid port pids
+    local pidPorts=( )
+    oldIFS="$IFS"
+    #allVnc=`sudo netstat -nptl 2>/dev/null | grep ":59[0-9][0-9]"`
+    tmp=`$lnetstat -nptl 2>/dev/null`
 
-	IFS=$'\n'
-	allVnc=`echo "$tmp" | grep ":59[0-9][0-9]"`
-	for vnc in $allVnc;do
-		unset pid port
-		pid=$(echo $vnc | awk '{print $7}' | sed 's#\([0-9]\+\)/.*#\1#')
-		port=$(echo $vnc | awk '{print $4}' | sed 's/.*:\(59[0-9][0-9]\)/\1/')
-		pids="$pid $pids" 
-		pidPorts[$pid]=$port	
-	done
-	IFS="$oldIFS"
+    IFS=$'\n'
+    allVnc=`echo "$tmp" | grep ":59[0-9][0-9]"`
+    for vnc in $allVnc;do
+        unset pid port
+        pid=$(echo $vnc | awk '{print $7}' | sed 's#\([0-9]\+\)/.*#\1#')
+        port=$(echo $vnc | awk '{print $4}' | sed 's/.*:\(59[0-9][0-9]\)/\1/')
+        pids="$pid $pids" 
+        pidPorts[$pid]=$port    
+    done
+    IFS="$oldIFS"
 
-	#trim ^\s and \s$
-	pids=$(echo $pids | sed 's/^\s\?\([0-9]\+\)\s\?$/\1/')
-	for pid in $pids;do
-		count=$(cat /proc/$pid/cmdline | grep "domain-[0-9]\+-${domain:0:20}" -c)
-		if [ $count -gt 0 ];then
-			echo ${pidPorts[$pid]}
-			match="yes"
-			break;
-		fi
-	done
-	[ $match ] || { echo -e "\033[1;31m\t\terror!!!not found vnc port for $domain,exit -2\033[0m" && exit -2; }
+    #trim ^\s and \s$
+    pids=$(echo $pids | sed 's/^\s\?\([0-9]\+\)\s\?$/\1/')
+    for pid in $pids;do
+        count=$(cat /proc/$pid/cmdline | grep "domain-[0-9]\+-${domain:0:20}" -c)
+        if [ $count -gt 0 ];then
+            echo ${pidPorts[$pid]}
+            match="yes"
+            break;
+        fi
+    done
+    [ $match ] || { echo -e "\033[1;31m\t\terror!!!not found vnc port for $domain,exit -2\033[0m" && exit -2; }
 }
 
 function doVncViewer(){
-	#top left 10 20
-	#top right 1330 10
-	i3-msg 'floating toggle ;resize set 270 200;move position 1330 00' >/dev/null 2>&1
-	$lchmod a+rw $logFile
-	local vncPort=""
-	vncPort=$(gotDomainVncPort)
-	echo "vncPort: $vncPort"
-	vncviewer :$vncPort   >/dev/null 2>&1
+    #top left 10 20
+    #top right 1330 10
+    i3-msg 'floating toggle ;resize set 270 200;move position 1330 00' >/dev/null 2>&1
+    $lchmod a+rw $logFile
+    local vncPort=""
+    vncPort=$(gotDomainVncPort)
+    echo "vncPort: $vncPort"
+    vncviewer :$vncPort   >/dev/null 2>&1
 
-	local timeWait=0
-	local sliceWait=1
-	while [ 0 ];do
-		hereWS=$(getCurWorkSpace)
-		if [ $hereWS -eq $curWS ];then
-			break
-		else
-			sleep $sliceWait
-			let timeWait+=$sliceWait
-			echo "had wait for focus: $timeWait seconds"
-		fi
-	done
-	i3-msg 'floating toggle'  >/dev/null 2>&1
+    local timeWait=0
+    local sliceWait=1
+    while [ 0 ];do
+        hereWS=$(getCurWorkSpace)
+        if [ $hereWS -eq $curWS ];then
+            break
+        else
+            sleep $sliceWait
+            let timeWait+=$sliceWait
+            echo "had wait for focus: $timeWait seconds"
+        fi
+    done
+    i3-msg 'floating toggle'  >/dev/null 2>&1
 }
 
 function getCurWorkSpace(){
-	pyName=/tmp/getFocusWorkSpace.py
+    pyName=/tmp/getFocusWorkSpace.py
 cat << EOF > $pyName
 #!/bin/env python
 
@@ -117,66 +117,66 @@ for d in dicts:
         print d['num']
 EOF
 
-	chmod a+x $pyName
-	curWorkSpace=$($pyName)
-	echo $curWorkSpace
-	rm $pyName
+    chmod a+x $pyName
+    curWorkSpace=$($pyName)
+    echo $curWorkSpace
+    rm $pyName
 }
 
 function checkXml(){
-	[ -f $xmlConfig ] && return
-	verbose echo -e "\033[1;33m\t\tauto gen $xmlConfig\033[0m"
-	cp $xmlTemplate $xmlConfig
-	local uuid=$(uuidgen) 
-	verbose echo "$TAB random uuid\t:$uuid"
+    [ -f $xmlConfig ] && return
+    verbose echo -e "\033[1;33m\t\tauto gen $xmlConfig\033[0m"
+    cp $xmlTemplate $xmlConfig
+    local uuid=$(uuidgen) 
+    verbose echo "$TAB random uuid\t:$uuid"
 #kvm uuid
     sed -i "s#<uuid>\S\+</uuid>#<uuid>$uuid</uuid>#" $xmlConfig
-	#sed -i "#<uuid>\S\+</uuid># d" $xmlConfig
-#kvm name	
-	sed -i "s#<name>\S\+</name>#<name>$domain</name>#" $xmlConfig
+    #sed -i "#<uuid>\S\+</uuid># d" $xmlConfig
+#kvm name    
+    sed -i "s#<name>\S\+</name>#<name>$domain</name>#" $xmlConfig
 #kvm source file
-	sed -i "s#\(\s\+<source file=\)\s*\S\+.img'/>#\1\'$imgDisk\'/>#" $xmlConfig 
-	sed -i "s#\(\s\+<source file=\)\s*\S\+.iso'>#\1\'$domainIso\'>#" $xmlConfig
-	sed -i "s#\(\s\+<source path=\)\s*\S\+.log'/>#\1\'$logFile\'/>#" $xmlConfig
+    sed -i "s#\(\s\+<source file=\)\s*\S\+.img'/>#\1\'$imgDisk\'/>#" $xmlConfig 
+    sed -i "s#\(\s\+<source file=\)\s*\S\+.iso'>#\1\'$domainIso\'>#" $xmlConfig
+    sed -i "s#\(\s\+<source path=\)\s*\S\+.log'/>#\1\'$logFile\'/>#" $xmlConfig
 #kvm vnc port autoport
-	#skip
+    #skip
 #kvm mac
-	local macStr=$(echo `sed -n '/<mac address/p' $xmlConfig`)
-	macStr=${macStr##\<mac address=\'}
-	macStr=${macStr%%\'/>}
-	local macHex="0x$(echo $macStr | sed -n 's/://gp')"
-	verbose echo "$TAB oldmacHex\t:$macHex"
-	local newMac=$(awk "BEGIN{num=strtonum($macHex);printf(\"%X\n\",num+$RANDOM)}")
-	verbose echo "$TAB newMacHex\t:$newMac"
-	newMacStr=$(echo $newMac | sed -r 's/^([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})/\1:\2:\3:\4:\5:\6/')
-	verbose echo "$TAB newMacStr\t:$newMacStr"
-	sed -i "s#\(\s\+<mac address=\)\s*\S\+'/>#\1\'$newMacStr\'/>#" $xmlConfig
+    local macStr=$(echo `sed -n '/<mac address/p' $xmlConfig`)
+    macStr=${macStr##\<mac address=\'}
+    macStr=${macStr%%\'/>}
+    local macHex="0x$(echo $macStr | sed -n 's/://gp')"
+    verbose echo "$TAB oldmacHex\t:$macHex"
+    local newMac=$(awk "BEGIN{num=strtonum($macHex);printf(\"%X\n\",num+$RANDOM)}")
+    verbose echo "$TAB newMacHex\t:$newMac"
+    newMacStr=$(echo $newMac | sed -r 's/^([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})/\1:\2:\3:\4:\5:\6/')
+    verbose echo "$TAB newMacStr\t:$newMacStr"
+    sed -i "s#\(\s\+<mac address=\)\s*\S\+'/>#\1\'$newMacStr\'/>#" $xmlConfig
 }
 
 function checkImg(){
-	[ -f $imgDisk ] && return
-	verbose echo -e "\033[1,33m\t\tauto create $imgDisk\033[0m" 
-	qemu-img create $imgDisk $imgSizeWhenAutoCreate
+    [ -f $imgDisk ] && return
+    verbose echo -e "\033[1,33m\t\tauto create $imgDisk\033[0m" 
+    qemu-img create $imgDisk $imgSizeWhenAutoCreate
 }
 
 function check(){
-	checkImg && checkXml
+    checkImg && checkXml
 }
 
 function main(){
-	#check param
-	if [ $# -lt 1 ];then
-		Usage
-		exit -1;
-	fi	
-	check || exit
-	curWS=$(getCurWorkSpace)
-	echo "domain: $domain"
-	echo "xmlConfig: $xmlConfig"
-	echo "curWorkSpace: $curWS"
-	domainCreate
-	doVncViewer
-	$lvirsh destroy $domain
+    #check param
+    if [ $# -lt 1 ];then
+        Usage
+        exit -1;
+    fi    
+    check || exit
+    curWS=$(getCurWorkSpace)
+    echo "domain: $domain"
+    echo "xmlConfig: $xmlConfig"
+    echo "curWorkSpace: $curWS"
+    domainCreate
+    doVncViewer
+    $lvirsh destroy $domain
 }
 
 #main
@@ -196,19 +196,19 @@ domainIso=${workdir}${domain}.iso
 
 uri="system"
 if [ "$uri" == "system" ];then
-	#TODO
-	domainSavedDir="/home/$USER/.config/libvirt/qemu/save/"
-	domainSaved=$domainSavedDir$domain.save
+    #TODO
+    domainSavedDir="/home/$USER/.config/libvirt/qemu/save/"
+    domainSaved=$domainSavedDir$domain.save
 
-	lvirsh="sudo virsh"
-	lnetstat="sudo netstat"
-	lchmod="sudo chmod"
+    lvirsh="sudo virsh"
+    lnetstat="sudo netstat"
+    lchmod="sudo chmod"
 else
-	domainSavedDir="/home/$USER/.config/libvirt/qemu/save/"
-	domainSaved=$domainSavedDir$domain.save
+    domainSavedDir="/home/$USER/.config/libvirt/qemu/save/"
+    domainSaved=$domainSavedDir$domain.save
 
-	lvirsh="virsh"
-	lnetstat="netstat"
-	lchmod="chmod"
+    lvirsh="virsh"
+    lnetstat="netstat"
+    lchmod="chmod"
 fi
 main "$@"
--- kvm/vmUsb.sh
+++ kvm/vmUsb.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 function Usage(){
-	echo "need param"
-	echo "Usage: $program 'domain'"
+    echo "need param"
+    echo "Usage: $program"
+    echo "Usage: $program detach"
 }
 
 function verbose(){
@@ -28,7 +29,7 @@ function lerror(){
 }
 
 function lsudo(){
-	#green
+    #green
     echo -e "\033[1;32msudo for:"\""$@"\""\033[0m"
     sudo "$@"
 }
@@ -43,142 +44,145 @@ function callFunc(){
 
 
 function isDigital(){
-	[ -n "$(echo $1 | sed -n "/^[0-9]\+$/p")" ] && echo "true"
-	return 0
+    [ -n "$(echo $1 | sed -n "/^[0-9]\+$/p")" ] && echo "true"
+    return 0
 }
 
 function listSelect(){
-	local name=$1[@]
-	local arrary=("${!name}")
-	local min=0
-	local let max=$((${#arrary[@]}-1))
-	local sel=-1
-	
-	for item in "${arrary[@]}";do
-		echo "member: $item"
-	done
+    local name=$1[@]
+    local arrary=("${!name}")
+    local min=0
+    local let max=$((${#arrary[@]}-1))
+    local sel=-1
+    
+    for i in "${!arrary[@]}";do
+	echo "memer [$i]: ${arrary[$i]}"
+    done
+    #for item in "${arrary[@]}";do
+        #echo "member: $item"
+    #done
 
-	if [ $max -lt 0 ];then
-		verbose "none member,return -1"
-		return -1
-	elif [ $max -eq 0 ];then
-		sel=0
-		verbose "members just one"
-	fi
+    if [ $max -lt 0 ];then
+        verbose "none member,return -1"
+        return -1
+    elif [ $max -eq 0 ];then
+        sel=0
+        verbose "members just one"
+    fi
 
-	while [ -z "$sel" ]  || ! [ $(isDigital $sel) ] || \
-		  [ "$sel" -gt $max ] || [ "$sel" -lt $min ]; do
+    while [ -z "$sel" ]  || ! [ $(isDigital $sel) ] || \
+          [ "$sel" -gt $max ] || [ "$sel" -lt $min ]; do
 
-		echo -n "chose [${min}-$max]: "
-		read sel
-	done
-	verbose "select:${arrary[$sel]}"
+        echo -n "chose [${min}-$max]: "
+        read sel
+    done
+    verbose "select:${arrary[$sel]}"
 
-	return $sel
+    return $sel
 }
 
 function domainSelec(){
-	#local virshLines=$(virsh -c qemu:///system list --all | sed -n '3,$ p')
-	local virshLines=$(virsh -c qemu:///system list --all | grep 'running')
-	local oldIFS="$IFS"
-	IFS=$'\n'
-	local domains=( )
-	local index=0
-	for line in $virshLines;do
-		[ -z $line ] && continue
-		domain=$(echo $line | awk '{print $2}')
-		domains[$index]=$domain
-		let index+=1
-	done
-	IFS="$oldIFS"
+    #local virshLines=$(virsh -c qemu:///system list --all | sed -n '3,$ p')
+    local virshLines=$(virsh -c qemu:///system list --all | grep 'running')
+    local oldIFS="$IFS"
+    IFS=$'\n'
+    local domains=( )
+    local index=0
+    for line in $virshLines;do
+        [ -z $line ] && continue
+        domain=$(echo $line | awk '{print $2}')
+        domains[$index]=$domain
+        let index+=1
+    done
+    IFS="$oldIFS"
 
-	listSelect domains
-	index=$?
-	if [ $index -eq 255 ];then
-		lerror "no domain found"	
-	fi
-	selectedDomain=${domains[$index]}
-	verbose $selectedDomain
+    listSelect domains
+    index=$?
+    if [ $index -eq 255 ];then
+        lerror "no domain found"    
+    fi
+    selectedDomain=${domains[$index]}
+    verbose $selectedDomain
 }
 
 usbReg=${workDir}usbDev-*.xml
 function usbSelecByFiles(){
-	local usbs=( )
-	local index=0
-	for f in ${usbReg};do
-		[ -f $f ] || continue
-		verbose "found $f"
-		usbs[$index]=`basename $f`
-		let index+=1
-	done
+    local usbs=( )
+    local index=0
+    for f in ${usbReg};do
+        [ -f $f ] || continue
+        verbose "found $f"
+        usbs[$index]=`basename $f`
+        let index+=1
+    done
 
-	verbose "found arrary ${usbs[@]}"
+    verbose "found arrary ${usbs[@]}"
 
-	listSelect usbs
-	index=$?
-	if [ $index -eq 255 ];then
-		lerror "no usb device found"	
-	fi
-	selectedUsb=${usbs[$index]}
-	verbose $selectedUsb
+    listSelect usbs
+    index=$?
+    if [ $index -eq 255 ];then
+        lerror "no usb device found"    
+    fi
+    selectedUsb=${usbs[$index]}
+    verbose $selectedUsb
 }
 
 function usbSelecByDominAttached(){
-	local domain=$1
-	usbDevices=$(virsh -c qemu:///system dumpxml $domain | grep -A 3 "<hostdev mode='subsystem' type='usb'")
-	echo $usbDevices
-	vendors=($(echo $usbDevices | grep -oP "(?<=vendor id=')[^']+"))
-	products=($(echo $usbDevices | grep -oP "(?<=product id=')[^']+"))
-	
-	echo ${vendors[@]}
-	echo ${products[@]}
+    local domain=$1
+    usbDevices=$(virsh -c qemu:///system dumpxml $domain | grep -A 3 "<hostdev mode='subsystem' type='usb'")
+    echo $usbDevices
+    vendors=($(echo $usbDevices | grep -oP "(?<=vendor id=')[^']+"))
+    products=($(echo $usbDevices | grep -oP "(?<=product id=')[^']+"))
+    
+    echo ${vendors[@]}
+    echo ${products[@]}
 
-	local vIndex=0
-	local matcheds=( )
-	local mIndex=0
-	for vid in "${vendors[@]}";do
-		pid=${products[$vIndex]}
-		verbose "vid:$vid,pid:$pid"
-		for f in ${usbReg};do
-			[ -f $f ] || continue
-			verbose "try match $f"
-			if [ $(cat $f | grep -o "$vid") ] && [ $(cat $f | grep -o "$pid") ];then
-				verbose "$f matched for $vid:$pid"
-				matcheds[$mIndex]=$(basename $f)
-				let mIndex+=1
-			fi
-		done
-		let vIndex+=1
-	done
-	echo ${matcheds[@]}
+    local vIndex=0
+    local matcheds=( )
+    local mIndex=0
+    for vid in "${vendors[@]}";do
+        pid=${products[$vIndex]}
+        verbose "vid:$vid,pid:$pid"
+        for f in ${usbReg};do
+            [ -f $f ] || continue
+            verbose "try match $f"
+            if [ $(cat $f | grep -o "$vid") ] && [ $(cat $f | grep -o "$pid") ];then
+                verbose "$f matched for $vid:$pid"
+                matcheds[$mIndex]=$(basename $f)
+                let mIndex+=1
+            fi
+        done
+        let vIndex+=1
+    done
+    echo ${matcheds[@]}
 
-	listSelect matcheds 
-	index=$?
-	if [ $index -eq 255 ];then
-		lerror "no usb device found"	
-	fi
-	selectedUsb=${matcheds[$index]}
-	verbose $selectedUsb
+    listSelect matcheds 
+    index=$?
+    if [ $index -eq 255 ];then
+        lerror "no usb device found"    
+    fi
+    selectedUsb=${matcheds[$index]}
+    verbose $selectedUsb
 }
 
 function attach(){
-	usbSelecByFiles
-	local ret=$(virsh -c qemu:///system attach-device $selectedDomain ${workDir}${selectedUsb} 2>&1)
-	if [ "$(echo $ret | grep 'error')" ];then
-		lerror $ret
-	else
-		lsuccess $ret
-	fi
+    usbSelecByFiles
+    local ret=$(virsh -c qemu:///system attach-device $selectedDomain ${workDir}${selectedUsb} 2>&1)
+    if [ "$(echo $ret | grep 'error')" ];then
+        lerror $ret
+    else
+        lsuccess $ret
+    fi
 }
 
 function detach(){
-	usbSelecByDominAttached $selectedDomain
-	local ret=$(virsh -c qemu:///system detach-device $selectedDomain $selectedUsb 2>&1)
-	if [ "$(echo $ret | grep 'error')" ];then
-		lerror $ret
-	else
-		lsuccess $ret
-	fi
+    usbSelecByDominAttached $selectedDomain
+    local ret=$(virsh -c qemu:///system detach-device $selectedDomain $selectedUsb 2>&1)
+    if [ "$(echo $ret | grep 'error')" ];then
+        lerror $ret
+    else
+        lsuccess $ret
+    fi
 }
 
 #main
@@ -189,7 +193,7 @@ workDir="$PWD/"
 TAB="-e \t"
 domainSelec
 if [ $1 ];then
-	detach
+    detach
 else
-	attach
+    attach
 fi
