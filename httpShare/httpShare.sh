d104f7212bec51ccad5aff346d5e8c87552d6609
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-02-15 15:21:52 +0800

    1,enable upload   httpShare.sh s for download sg for upload  or httpShare.sh for both

--- httpShare/httpShare.sh
+++ httpShare/httpShare.sh
@@ -1,8 +1,35 @@
 #/bin/bash
-port=8000
-if [ $# -ge 1 ];then
-    port=$1
-    #echo "Use port: $port"
-fi
 
-python -m SimpleHTTPServer $port
+port=8000
+prog=$0
+
+while [ $# -gt 0 ];do
+    case "$1" in
+	s)
+	    strict=true
+	    ;;
+	g)
+	    get=true
+	    ;;
+	sg)
+	    strict=true
+	    get=true
+	    ;;
+    esac
+    shift
+done
+
+[ -L $prog ] && tDir=`dirname $(readlink $prog)`/
+
+if [ $strict ];then
+    if [ $get ];then
+	echo "--------upload files--------"
+	python ${tDir}upload.py
+    else
+	echo "--------download files--------"
+	python -m SimpleHTTPServer $port
+    fi
+else
+    echo "--------upload/download files--------"
+    python ${tDir}SimpleHTTPServerWithUpload.py
+fi
