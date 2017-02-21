9414da98f120cc880574417879e3a63740a78669
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-02-21 10:23:08 +0800

    synergyX for proxy server or client

---
+++ synergyX.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+prog=$0
+svrName="rowanInspur.lan"
+
+svr="$(dirname $prog)/synergys"
+cli="$(dirname $prog)/synergyc $svrName"
+
+
+if [ $HOSTNAME == $svrName ];then
+    echo "server $svr"
+    exec $svr
+else
+    echo "client $cli"
+    exec $cli
+    #for synergyc disable all xkbmap,just use the server config.
+    /usr/bin/setxkbmap -option -option
+fi
