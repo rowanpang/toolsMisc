65aaad2c24ee1abdd47f48ac1bdc7909091e9d29
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-04-10 19:31:50 +0800

    1,add crond task every 6h update the shadowsocks.

--- shadowsocks/shadowsocks-osInit.sh
+++ shadowsocks/shadowsocks-osInit.sh
@@ -2,8 +2,14 @@
 source ./osInitframe//lib.sh
 
 function ssInit(){
+    dir=$localdir
     pkgCheckInstall python-shadowsocks
     pkgCheckInstall libsodium
+
+    #cron,run 5 minutes after midn,6,12am ...,everyday"
+    lsudo sh -c "cat << EOF > /etc/cron.d/shadowSocks-update
+2 0-23/6 * * * pangwz ${dir}ssStart.sh
+EOF"
 }
 
 ssInit
