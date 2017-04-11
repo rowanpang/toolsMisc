11cdedb72c693b0bf8956e3768b11c83fa46214a
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-04-11 16:26:27 +0800

    1,git repo init script update.

---
+++ justScript/repoGit-osInit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source ./osInitframe/lib.sh
+
+function repoNoteGit(){
+    local workDir="$HOME/noteGit" 
+
+    if [ -d $workDir ];then
+	git -C $workDir pull
+    else
+	git -C $HOME clone git@github.com:rowanpang/noteGit.git
+    fi
+}
+
+function repoConan(){
+    local workDir="$HOME/conan" 
+
+    if [ -d $workDir ];then
+	git -C $workDir pull
+    else
+	git -C $HOME clone git@git.oschina.net:rowanPang/conan.git
+    fi
+}
+
+repoNoteGit
+repoConan
