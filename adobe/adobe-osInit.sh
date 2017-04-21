3e66f1f52d27044a9dc745b346f45d0a2d845395
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-04-22 01:21:54 +0800

    add adobe flash

---
+++ adobe/adobe-osInit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ./osInitframe/lib.sh
+
+function baseInit(){
+    local dir=$localdir
+    if ! [ $(pkgInstalled adobe-release-x86_64) ];then
+	lsudo rpm -i ${dir}adobe-release-x86_64-1.0-1.noarch.rpm
+    fi
+
+    pkgCheckInstall flash-plugin
+
+}
+
+baseInit
