0adfd63e54ed33123e9652b29759af769fa04a4a
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-06-08 14:09:58 +0800

    1,pkg repo configure ,default disable rpmfusion and othters.

--- adobe/adobe-osInit.sh
+++ adobe/adobe-osInit.sh
@@ -8,8 +8,8 @@ function baseInit(){
 	lsudo rpm -i ${dir}adobe-release-x86_64-1.0-1.noarch.rpm
     fi
 
+    lsudo dnf config-manager --set-disabled adobe-linux-x86_64
     pkgCheckInstall flash-plugin adobe-linux-x86_64
-
 }
 
 baseInit
