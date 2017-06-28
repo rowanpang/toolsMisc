429f05a9dd9281eade9c665c511e991e35366a91
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-06-28 16:05:36 +0800

    add usage help info

--- httpShare/httpShare.sh
+++ httpShare/httpShare.sh
@@ -4,21 +4,35 @@ port=8000
 prog=$0
 both=true
 
+function usage(){
+    echo "
+	-h: for this help mesg	
+	 p: put file,share files for download
+	 g: get file,allow upload files
+	pg: put and get 
+	-p: port  default is $port  just useful with p/g/pg
+	"
+    exit
+}
+
 [ -L $prog ] && tDir=`dirname $(readlink $prog)`/
 
 while [ $# -gt 0 ];do
     case "$1" in
+	-h)
+	    usage
+	    ;;
+	-p)
+	    port=$2
+	    shift
+	    ;;
 	p)
 	    put=true
 	    both=false
-	    echo "--------download files--------"
-	    python -m SimpleHTTPServer $port
 	    ;;
 	g)
 	    get=true
 	    both=false
-	    echo "--------upload files--------"
-	    python ${tDir}upload.py
 	    ;;
 	pg)
 	    both=true
@@ -27,7 +41,13 @@ while [ $# -gt 0 ];do
     shift
 done
 
-if [ $both == "true" ];then
+if [ "$put" ];then
+    echo "--------download files--------"
+    python -m SimpleHTTPServer $port
+elif [ "$get" ];then
+    echo "--------upload files--------"
+    python ${tDir}upload.py
+elif [ "$both" == "true" ];then
     echo "--------upload/download files--------"
     python ${tDir}SimpleHTTPServerWithUpload.py
 fi
