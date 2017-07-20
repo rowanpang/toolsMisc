70a7aae1fbe1637260792ae17bf09d5a12527038
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-07-20 11:22:53 +0800

    1,chang ycm debug level to error 2,init ycm dependence when do osInit

--- vim/vim-osInit.sh
+++ vim/vim-osInit.sh
@@ -11,6 +11,9 @@ function initVim(){
     ln -snf $dir ${HOMEDIR}.vim
     lsudo ln -snf $dir ${ROOTHOME}.vim   #for root vim
     lsudo sed  -i 's; \[\s\+.*\]\s\+\&\&\s\+return$;#&;' /etc/profile.d/vim.sh
+
+    #for ycm dependence
+	pkgsInstall clang llvm automake gcc gcc-c++ kernel-devel cmake python-devel
 }
 
 initVim
