659f883d04d61d7b210179bca348e768799dbad3
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-08-18 20:21:59 +0800

    [synergy]\n change layou to lrwr

--- synergy/synergy-osInit.sh
+++ synergy/synergy-osInit.sh
@@ -2,9 +2,31 @@
 
 source ./osInitframe/lib.sh
 
+#just init configfile, start ./synergyX.sh by i3
 function initSynergy(){
     local dir=$localdir
-    lsudo ln -sf ${dir}synergy.conf /etc/synergy.conf
+    local conf='ll'		#linux on left
+    local conf='lu'		#linux on up
+    local conf='lr'		#linux on right 
+    tconf = "$HOME/.synergy.conf"
+
+    case $conf in
+	'll')
+	    lsudo ln -sf ${dir}synergy.conf.llwr  /etc/synergy.conf
+	    ln -sf ${dir}synergy.conf.llwr  $tconf
+	    ;;
+	'lr')
+	    lsudo ln -sf ${dir}synergy.conf.lrwl  /etc/synergy.conf
+	    ln -sf ${dir}synergy.conf.lrwl $tconf
+	    ;;
+	'lu')
+	    lsudo ln -sf ${dir}synergy.conf.luwd  /etc/synergy.conf
+	    ln -sf ${dir}synergy.conf.luwd $tconf
+	    ;;
+	*)
+	    return -1
+	    ;;
+    esac
 }
 
 initSynergy
