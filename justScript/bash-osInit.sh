a7b5965ec1cb1a6b111daf2a45eeac2d2de85816
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-08-18 17:08:48 +0800

    1,export SVN_EDITOR=vim 2,ycm  identifiers from string and comment

--- justScript/bash-osInit.sh
+++ justScript/bash-osInit.sh
@@ -12,3 +12,7 @@ fi
 if [ `grep -c gpg-connect-agent $bashrc` -eq 0 ];then
     sed -i '$igpg-connect-agent /bye' $bashrc
 fi
+
+if [ `grep -c SVN_EDITOR $bashrc` -eq 0 ];then
+    sed -i '$iexport SVN_EDITOR=vim' $bashrc
+fi
