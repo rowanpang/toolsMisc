bbf8a7d68e2d9b26ec1a738023001aaebc39a3bb
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-08-30 13:29:08 +0800

    [tsocks]     add '-h' for usage info     add '-s','-p' for select server and port     add tsocks.conf as confiure template

--- tsocks/tsocks-osInit.sh
+++ tsocks/tsocks-osInit.sh
@@ -4,18 +4,22 @@ source ./osInitframe/lib.sh
 
 function main(){
     pkgCheckInstall tsocks
+
+    [ -d $HOME/.tsocks ] || mkdir -p $HOME/.tsocks
+
     if ! [ -x "/usr/bin/tsocks.org" ];then
 	lsudo mv /usr/bin/tsocks /usr/bin/tsocks.org
-	lsudo cp $localdir/tsocks /usr/bin/tsocks
     fi
+    lsudo cp -rf $localdir/tsocks /usr/bin/tsocks
+
     lib="/usr/lib64/libtsocks.so.1.8"
     libbk="${lib}.org"
-    
-
     if [ -e $lib ] && ! [ -e $libbk ];then
 	lsudo mv $lib $libbk
-	lsudo cp $localdir/libtsocks.so.1.8 $lib
     fi
+    lsudo cp -rf $localdir/libtsocks.so.1.8 $lib
+
+    cp -rf $localdir/tsocks.conf $HOME/.tsocks/conf
 }
 
 main
