2cb10a5fce46227b6bcfc26f07f5e0c5df5ab74e
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-09-21 10:32:31 +0800

    [synergy]     if not in rowanNet listen to default route link

--- synergy/synergyX.sh
+++ synergy/synergyX.sh
@@ -2,12 +2,13 @@
 prog=$0
 svrName="rowanInspur.lan"
 svrPort=24800
-isRowanNet=`ip a s | grep 'inet ' | grep '192.168'`
-ipBridge0=`ip a s bridge0 | grep 'inet\s\+' | awk '{print $2}' | cut -d '/' -f 1`
+isRowanNet=`ip a s | grep 'inet ' | grep '192\.168\.1\.'`
+deflink=`ip route | grep default | sed 's/^.*dev //' | awk '{print $1}'`
+ipBridge0=`ip a s $deflink | grep 'inet\s\+' | awk '{print $2}' | cut -d '/' -f 1`
 if [ "$isRowanNet" ];then
     addr='0.0.0.0'
 else
-    exit
+    #exit
     addr=$ipBridge0
 fi
 
@@ -15,7 +16,6 @@ svrCmd="$(dirname $prog)/synergys --address $addr"
 cliCmd="$(dirname $prog)/synergyc $svrName"
 
 #by startup the network may not connected.
-
 if [ $HOSTNAME == $svrName ];then
     $svrCmd
 else
