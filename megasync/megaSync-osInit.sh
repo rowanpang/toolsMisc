9867a093f22f045eb0be783a9c69f538d6653150
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-09-27 10:21:47 +0800

    [crunch]     install the $(osVer) repofile [megasync]     add more note msg

--- megasync/megaSync-osInit.sh
+++ megasync/megaSync-osInit.sh
@@ -3,11 +3,13 @@
 source ./osInitframe/lib.sh
 
 function main(){
+    #dns error under inspur net so add hosts
     hosts='/etc/hosts'
     if [ $(grep -c 'mega.nz' $hosts) -eq 0 ];then
 	lsudo sed -i "/::1/a154.53.224.166 mega.nz" $hosts
     fi
 
+    #install rpm will add a repo for yum for the fedora_$(osVer)
     pkgName="megasync"
     pkg="https://mega.nz/linux/MEGAsync/Fedora_${osVer}/$(uname -m)/$pkgName-Fedora_${osVer}.x86_64.rpm"
     netRPMInstall $pkgName $pkg
