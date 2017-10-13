a568bcec4e77bf1e0e03948372274f06bc194139
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-10-13 10:51:08 +0800

    [svn]     add default config for svn

--- svn/svn-osInit.sh
+++ svn/svn-osInit.sh
@@ -4,6 +4,10 @@ source ./osInitframe/lib.sh
 
 function main(){
     pkgCheckInstall svn
+    svn --version --quiet
+    lcfg=${localdir}config
+    tcfg=$HOME/.subversion/config
+    ln -sf $lcfg $tcfg
 }
 
 main
