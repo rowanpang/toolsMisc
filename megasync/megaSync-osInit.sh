6f17f859aac750bd15ea76d1db62f4aa8cd6e6e9
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-11-07 23:16:57 +0800

    [mega]     add dependence for cryptopp

--- megasync/megaSync-osInit.sh
+++ megasync/megaSync-osInit.sh
@@ -9,6 +9,8 @@ function main(){
 	lsudo sed -i "/::1/a154.53.224.166 mega.nz" $hosts
     fi
 
+    pkgCheckInstall cryptopp
+
     #install rpm will add a repo for yum for the fedora_${osVer}
     pkgName="megasync"
     pkg="https://mega.nz/linux/MEGAsync/Fedora_${osVer}/$(uname -m)/$pkgName-Fedora_${osVer}.x86_64.rpm"
