0bdba580cce77a3cda870cee953cfc0f95252d4c
Rowan Pang <pangweizhen.2008@hotmail.com>
2017-11-14 10:01:02 +0800

    [main]     sync repo before do init

--- main.sh
+++ main.sh
@@ -28,9 +28,11 @@ function initRepo(){
 
 	[ $(cat /etc/dnf/dnf.conf | grep -c 'expire=1000d') -ge 1 ] || lsudo sed -i "$ a metadata_expire=1000d" /etc/dnf/dnf.conf
 	[ $(cat /etc/dnf/dnf.conf | grep -c 'keepcache=true') -ge 1 ] || lsudo sed -i "$ a keepcache=true" /etc/dnf/dnf.conf
+	[ -f /etc/yum.repos.d/FZUG.repo ] || lsudo dnf config-manager --add-repo=http://repo.fdzh.org/FZUG/FZUG.repo
+
+	dnf --refresh makecache
     fi
 
-    [ -f /etc/yum.repos.d/FZUG.repo ] || lsudo dnf config-manager --add-repo=http://repo.fdzh.org/FZUG/FZUG.repo
 }
 
 #$1,scripts regex to exec
