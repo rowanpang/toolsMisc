93ae47a9b24fd764f4f273b68029f0c8b041b4b3
Rowan Pang <pangweizhen.2008@hotmail.com>
2018-03-01 14:19:58 +0800

    [tolsmisc tripwire]     add tripwire and policy config file

--- security/misc-osInit.sh
+++ security/misc-osInit.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 source ./osInitframe/lib.sh
+function tripwireInit(){
+    local dir=$localdir
+    pkgCheckInstall tripwire
+
+    cp ${dir}/twpol.cfg /etc/tripwire/twpol.txt
+}
 
 function main(){
     #pkgCheckInstall hydra
+    tripwireInit
     true
 }
 
