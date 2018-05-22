f907fe607cd8bceb68598fca9db3b7382f701bb7
Rowan Pang <pangweizhen.2008@hotmail.com>
2018-05-22 17:19:49 +0800

    [graphviz]     change repo to fedora

--- graphic/graphic-osInit.sh
+++ graphic/graphic-osInit.sh
@@ -2,16 +2,21 @@
 source ./osInitframe/lib.sh
 
 function initGraphviz(){
-    repofile="/etc/yum.repos.d/graphviz-fedora.repo"
-    if ! [ -f $repofile ];then
-	lsudo wget --output-document $repofile  \
-		    http://www.graphviz.org/graphviz-fedora.repo
+    fedoraRepo="True"
+    if [ $fedoraRepo == "True" ];then
+	pkgsInstall graphviz graphviz-python graphviz-devel
+    else
+	repofile="/etc/yum.repos.d/graphviz-fedora.repo"
+	if ! [ -f $repofile ];then
+	    lsudo wget --output-document $repofile  \
+			http://www.graphviz.org/graphviz-fedora.repo
+	fi
+	lsudo sed -i 's/^enabled=1/enabled=0/' $repofile
+	pkgCheckInstall graphviz-qt.x86_64 graphviz-snapshot
+	pkgsInstall graphviz graphviz-doc graphviz-graphs
     fi
-    lsudo sed -i 's/^enabled=1/enabled=0/' $repofile
-    pkgCheckInstall graphviz-qt.x86_64 graphviz-snapshot
-    pkgsInstall graphviz graphviz-doc graphviz-graphs
-    sudo dot -c					    #gen configure
 
+    sudo dot -c					    #gen configure
     cp ${localdir}k柯南.jpg $HOME/Pictures
 }
 
