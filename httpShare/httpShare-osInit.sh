4aca55833242d292a8fb635e6f1e36b609f5ed61
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-03-21 11:49:22 +0800

    add phpfileShare init

--- httpShare/httpShare-osInit.sh
+++ httpShare/httpShare-osInit.sh
@@ -2,9 +2,31 @@
 
 source ./osInitframe/lib.sh
 
+function phpFileShare(){
+    phpDir="$HOME/filemanager"
+    php="$phpDir/filemanager.php"
+
+    user=kuxInspur
+    mypwd=kuxInspur
+    shDir="/home/smbPublic"
+
+
+    [ -d $phpDir ] || git clone https://github.com/alexantr/filemanager.git $phpDir
+    sed -i "s#'fm_admin' => 'fm_admin'#//&#; /\/\/'fm_admin' => 'fm_admin'/ a\    \'$user\' => \'$mypwd\'," $php
+
+    sed -i "/^\$root_path = \$_SERVER\[/ a \$root_path = \'$shDir\'" $php
+
+    httpConf="/etc/httpd/conf.d/phpFileShare.conf"
+
+    echo "Alias /index.php $php" > $httpConf
+
+}
+
 function main(){
     local dir=$localdir
     lsudo ln -sf ${dir}/httpShare.sh /usr/bin/httpShare.sh
+    phpFileShare
 }
 
 main
+
