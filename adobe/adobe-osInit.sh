1fb6a629548844993792bab28caa74aba0e6d0dd
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-04-26 10:32:33 +0800

    linux npapi flash player for firefox

--- adobe/adobe-osInit.sh
+++ adobe/adobe-osInit.sh
@@ -7,6 +7,9 @@ function baseInit(){
     if ! [ $(pkgInstalled adobe-release-x86_64) ];then
 	lsudo rpm -i ${dir}adobe-release-x86_64-1.0-1.noarch.rpm
     fi
+    if ! [ $(pkgInstalled flash-plugin) ];then
+	lsudo rpm -i ${dir}flash-player-npapi-32.0.0.171-release.x86_64.rpm
+    fi
 
     lsudo dnf config-manager --set-disabled adobe-linux-x86_64
     pkgCheckInstall flash-plugin adobe-linux-x86_64
---
+++ adobe/readme.txt
@@ -0,0 +1,11 @@
+0,basic
+    a,目前有两种adobe flash
+	NPAPI/PPAPI
+	PPAPI是新的,解决了一些安全问题.
+
+    b,
+	linux firefox 需要NPAPI
+	linux chrome 需要PPAPI
+
+    c,ref
+	https://blog.csdn.net/gatieme/article/details/56481893
