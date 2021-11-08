efeeb307c2e8747cf1d16de83f54acb1b1b054c4
Rowan Pang <pangweizhen.2008@qq.com>
2021-11-08 17:00:48 +0800

    support rm

--- shell/bin/myseco.sh
+++ shell/bin/myseco.sh
@@ -5,7 +5,8 @@ cd /usr/local/SecoClient
 #depends on 'sh /etc/init.d/SecoClientPromoteService.sh start'
 seccmd=./SecoClient
 seclink=ipsec_vnic
-ips="10.152.30.10 10.152.20.11"
+tgts="10.152.30.10 10.152.20.11 10.152.11.63 10.152.11.50"
+tgts="10.152.30.10 10.152.20.11 10.152.11.63"
 
 spid=`pgrep SecoClientPromo`
 if [ "X$spid" != "X" ];then
@@ -24,26 +25,42 @@ fi
 
 while :; do
     echo "try get $seclink gw"
-    gw=`ip r  |grep $seclink | awk '{ if(NR==2) print $NF}'`
+    gw=`ip r  |grep "dev $seclink.*link src" | awk '{print $NF}'`
     if [ "X$gw" != "X" ];then
-        echo "get $seclink successful: $gw"
+        echo "get $seclink gw successful: $gw"
         break;
     fi
     sleep 1
 done
+
+#curRT current rt for tgt
+curRT=(`ip r |grep "via $gw dev $seclink" | awk '{print $1}'`)
+echo "current rt via $gw dev $seclink: ${curRT[@]}"
 
 if [ `ip route |grep -c "default.*$seclink" ` -ge 1 ];then
     echo "delete default route via $gw $seclink"
     ip route delete to default dev $seclink
 fi
 
-for ip in $ips ;do
-    echo "try add sec route $ip/32 via $gw"
-    if [ `ip route |grep -c "$ip.*$seclink" ` -ge 1 ];then
-        echo "route for $ip is exist, ip r to check"
+for tgt in $tgts ;do
+    echo "try add sec route $tgt/32 via $gw"
+
+    if [[ ${curRT[@]/${tgt}/} != ${curRT[@]} ]];then
+        echo "route for $tgt is exist, ip r to check"
+        curRT=(${curRT[@]/${tgt}/})
         continue
     fi
-    ip route add to $ip/32 via $gw dev $seclink
+    ip route add to $tgt/32 via $gw dev $seclink
 done
+
+#delete unused rt
+rtTorm=${#curRT[@]}
+if [[ $rtTorm -ge 1 ]] && [[ "${curRT[0]}" != "default" ]];then
+    echo "need to delete rt for ${curRT[@]}"
+    for (( i=0; i < $rtTorm; i++));do
+        echo "delete rt for ${curRT[i]}/32 via $gw dev $seclink"
+        ip route del to ${curRT[i]}/32 via $gw dev $seclink
+    done
+fi
 
 cd -
